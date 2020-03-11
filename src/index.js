de7fdaaeef78ae2ba1b846b5613d49ee52a9d7d6
Vladimir Kochubenko <volodymyr.kochubenko@gmail.com>
2020-03-11 20:15:15 +0200

    portfolio

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {BrowserRouter} from "react-router-dom";
 const store = createStore(rootReducers);
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={"/"}>
             <App/>
         </BrowserRouter>
     </Provider>,
