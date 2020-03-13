e35149061098307a320d1b9c6f0fb025c64b3fd7
Vladimir Kochubenko <volodymyr.kochubenko@gmail.com>
2020-03-13 17:39:54 +0200

    portfolio

--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import {BlockContentWrapper} from "../components/Blocks"
+import {BlockContentWrapper, BlockInformationWrapper} from "../components/Blocks"
 import styled from "styled-components"
 import {TextContent} from "../components/Texts";
 
 const Wrapper = styled.div`
     max-width: 600px;
     margin: 0 auto;
+`;
+
+const Line = styled.p`
+    text-align: left;
+    margin: 10px 10px;
 `;
 
 export default class Experience extends React.PureComponent {
@@ -19,18 +24,24 @@ export default class Experience extends React.PureComponent {
     render() {
         return (
             <BlockContentWrapper>
-                <Wrapper>
+                <BlockInformationWrapper>
                     <h1>Technical Experience</h1>
-                    <p><TextContent size={"1.2rem"}><strong>Cross-platform:</strong> React Native, Cordova, Electron, Crosswalk, Tizen, WebOS.</TextContent></p>
-                    <p><TextContent size={"1.2rem"}><strong>DataBase:</strong> MongoDB, Google Firebase, MySQL, PostgreSQL, SQLite</TextContent></p>
-                    <p><TextContent size={"1.2rem"}><strong>JavaScript core:</strong> jQuery, Mocha, NPM, TypeScript, Yarn, Webpack. </TextContent></p>
-                    <p><TextContent size={"1.2rem"}><strong>JavaScript frameworks:</strong> Backbone.js, RAD.JS, React.js / React Native, </TextContent></p>
-                    <p><TextContent size={"1.2rem"}><strong>JavaScript Node.js:</strong> Express.js.</TextContent></p>
-                    <p><TextContent size={"1.2rem"}><strong>Markup:</strong> Bootstrap, CSS3, HTML5, Markdown, LESS, Sass, Styled Components</TextContent></p>
-                    <p><TextContent size={"1.2rem"}><strong>OS:</strong> GNU / Linux, OS X, Windows.</TextContent></p>
-                    <p><TextContent size={"1.2rem"}><strong>Version Control:</strong> Git, GitHub, GitLab.</TextContent></p>
-                    <p><TextContent size={"1.2rem"}><strong>Testing:</strong> Mocha, Jasmine.</TextContent></p>
-                </Wrapper>
+                    <Line><TextContent size={"1.5rem"}><strong>Cross-platform:</strong> React Native, Cordova, Electron,
+                        Crosswalk, Tizen, WebOS.</TextContent></Line>
+                    <Line><TextContent size={"1.5rem"}><strong>DataBase:</strong> MongoDB, Google Firebase, MySQL,
+                        PostgreSQL, SQLite</TextContent></Line>
+                    <Line><TextContent size={"1.5rem"}><strong>JavaScript core:</strong> jQuery, Mocha, NPM, TypeScript,
+                        Yarn, Webpack. </TextContent></Line>
+                    <Line><TextContent size={"1.5rem"}><strong>JavaScript frameworks:</strong> Backbone.js, RAD.JS,
+                        React.js / React Native, </TextContent></Line>
+                    <Line><TextContent size={"1.5rem"}><strong>JavaScript Node.js:</strong> Express.js.</TextContent></Line>
+                    <Line><TextContent size={"1.5rem"}><strong>Markup:</strong> Bootstrap, CSS3, HTML5, Markdown, LESS,
+                        Sass, Styled Components</TextContent></Line>
+                    <Line><TextContent size={"1.5rem"}><strong>OS:</strong> GNU / Linux, OS X, Windows.</TextContent></Line>
+                    <Line><TextContent size={"1.5rem"}><strong>Version Control:</strong> Git, GitHub, GitLab.</TextContent>
+                    </Line>
+                    <Line><TextContent size={"1.5rem"}><strong>Testing:</strong> Mocha, Jasmine.</TextContent></Line>
+                </BlockInformationWrapper>
             </BlockContentWrapper>
         )
     }
